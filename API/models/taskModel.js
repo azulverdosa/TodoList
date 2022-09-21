d0f93ab556e594f1ba876d36b2225c2897a0e9f0
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-21 18:14:14 -0400

    updated TaskSchema

--- API/models/taskModel.js
+++ API/models/taskModel.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
+  id: { type: String },
   task: { type: String, required: true, maxLength: 500 },
   note: { type: String, required: true, maxLength: 500 },
+  completed: { type: Boolean, required: true },
 });
 
 const Task = mongoose.model('Task', TaskSchema);
