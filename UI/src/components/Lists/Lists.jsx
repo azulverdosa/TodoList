9ceb6027408d20471c4eea5abbf3ccd71d597f4c
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-21 18:18:18 -0400

    create Lists component

---
+++ UI/src/components/Lists/Lists.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddItemForm from './AddItem';
+
+const Lists = () => {
+  return (
+    <div style={{ margin: '20px' }}>
+      Your list of LISTS will go here
+      <AddItemForm />;
+    </div>
+  );
+};
+
+export default Lists;
