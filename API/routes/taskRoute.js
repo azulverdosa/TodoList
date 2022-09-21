b6ca14e2dc8ad9b25c4e9126b85dc72e05168cac
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-21 18:15:27 -0400

    change /list rout to /task route & added `/task/:taskId`.delete route

--- API/routes/taskRoute.js
+++ API/routes/taskRoute.js
@@ -3,19 +3,25 @@ const router = express.Router();
 const Task = require('../models/taskModel');
 
 router.route('/additem').post((req, res) => {
+  const id = req.body._id;
   const task = req.body.task;
   const note = req.body.note;
+  const completed = req.body.completed;
 
   const newTask = new Task({
+    id,
     task,
     note,
+    completed,
   });
 
   newTask.save();
 });
 
-router.route('/list').get((req, res) => {
+router.route('/task').get((req, res) => {
   Task.find().then((foundTasks) => res.json(foundTasks));
 });
+
+router.route(`/task/:taskId`).delete((req, res) => {});
 
 module.exports = router;
