df5291b641b732fa311f9998959fbbaa4358d76a
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-26 12:42:59 -0400

    raised state away from Task.jsx here

--- UI/src/components/Lists/Task.jsx
+++ UI/src/components/Lists/Task.jsx
@@ -1,44 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import urlJoin from 'url-join';
+import { nanoid } from 'nanoid';
 
-const TaskItem = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: '',
-      task: '',
-      note: '',
-      completed: false,
-    },
-  ]);
-
-  // const taskID = (task) => {
-  //   const taskID = task._id;
-  //   console.log(taskID);
-  // };
-
-  const handleDelete = (id) => {
-    // const remainingTasks = tasks.filter((task) => id !== task._id);
-    // alert('are you sure');
-    axios
-      .delete(urlJoin(process.env.REACT_APP_API_URL, 'task', id))
-      .then((res) => {
-        if (res.status !== 200) {
-          console.log(tasks);
-          // setTasks(remainingTasks);
-        } else {
-          throw res.status;
-        }
-      })
-      .catch((err) => {
-        console.log('error?', err);
-      });
-  };
-
-  const handleEdit = (id) => {
-    tasks.find((task) => id === task._id && console.log(task));
-  };
-
+const TaskItem = ({ tasks, setTasks }) => {
   useEffect(() => {
     axios
       .get(urlJoin(process.env.REACT_APP_API_URL, 'task'))
@@ -54,10 +19,29 @@ const TaskItem = () => {
       });
   }, []);
 
+  const handleDelete = (id) => {
+    axios
+      .delete(urlJoin(process.env.REACT_APP_API_URL, 'task', id))
+      .then((res) => {
+        if (res.status === 200) {
+          setTasks(res.data);
+        } else {
+          throw res;
+        }
+      })
+      .catch((err) => {
+        console.log('error?', err);
+      });
+  };
+
+  const handleEditItem = (id) => {
+    tasks.find((task) => id === task._id && console.log(task));
+  };
+
   return (
     <div>
       {tasks.map((task) => (
-        <ul key={task.task}>
+        <ul key={nanoid(10)}>
           <div>
             <input type="checkbox" />
             <label>{task.task}</label>
@@ -66,7 +50,7 @@ const TaskItem = () => {
           <div>
             <button
               onClick={() => {
-                handleEdit(task._id);
+                handleEditItem(task._id);
               }}
               className="ui compact icon floated button"
             >
