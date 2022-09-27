8c867c76d96323925cee946ae507a866ff5afba6
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-27 16:38:20 -0400

    add functionality

--- UI/src/components/User/Login.jsx
+++ UI/src/components/User/Login.jsx
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import urlJoin from 'url-join';
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    name: '',
+    password: '',
+  });
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [error, setError] = useState({ message: '' });
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    setLogin((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+    console.log(login);
+  };
+
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    if (login.name === '' || login.password === '') {
+      setError({ message: 'Feilds cannot be blank' });
+    }
+    setLoginStatus(true);
+  };
+
   return (
     <div className="ui large form error" style={{ margin: '20px' }}>
       <div className="two fields">
         <form style={{ padding: 30 }}>
           <div className="field">
             <label autoComplete="off">Name</label>
-            <input type="text" name="first-name" placeholder="First Name" />
+            <input
+              name="name"
+              value={login.name}
+              onChange={handleInputChange}
+              placeholder="Name"
+              type="text"
+            />
           </div>
           <br />
           <div className="field">
             <label autoComplete="off">Password</label>
-            <input type="password" />
+            <input
+              name="password"
+              value={login.password}
+              onChange={handleInputChange}
+              placeholder="Password"
+              type="password"
+            />
           </div>
           <br />
-          {/* <div>
+          <div>
             <div className="ui error message">
               {error.message && <div className="header">{error.message}</div>}
-            </div> */}
-          <button className="ui submit button">Login</button>
+            </div>
+          </div>
+          <br />
+          <button onClick={handleLoginClick} className="ui submit button">
+            Login
+          </button>
         </form>
       </div>
     </div>
@@ -27,71 +73,3 @@ const Login = () => {
 };
 
 export default Login;
-
-// import { useState } from 'react';
-// import { Navigate } from 'react-router-dom';
-
-// import { userDatabase } from '../../data/mockUserData';
-// import { findUserByName } from '../helpers/helpers';
-
-// const LoginForm = ({ loginUser }) => {
-//   const [userName, setUserName] = useState('');
-//   const [userPassword, setUserPassword] = useState('');
-//   const [loginStatus, setLoginStatus] = useState(false);
-//   const [error, setError] = useState({ message: '' });
-
-//   const handleNameInput = (event) => {
-//     setUserName(event.target.value);
-//   };
-
-//   const handlePasswordInput = (event) => {
-//     setUserPassword(event.target.value);
-//   };
-
-//   const handleLoginClick = (event) => {
-//     event.preventDefault();
-//     if (findUserByName(userName, userDatabase)) {
-//       // && confirmPassword(userPassword, userDatabase)
-//       setLoginStatus(true);
-//     } else if (userName === '' || userPassword === '') {
-//       setError({ message: 'Fields cannot be blank' });
-//     } else {
-//       setError({ message: 'No user found' });
-//     }
-//     // setUserName('');
-//     // setUserPassword('');
-//   };
-
-//   if (loginStatus) {
-//     return <Navigate replace to="/Lists" />;
-//   }
-
-//   return (
-//     <div className="ui large form error">
-//       <div className="two fields">
-//         <form style={{ padding: 30 }}>
-//           <div className="field">
-//             <label>User Name</label>
-//             <input value={userName} onChange={handleNameInput} type="text" />
-//           </div>
-//           <br />
-//           <div className="field">
-//             <label>Password</label>
-//             <input value={userPassword} onChange={handlePasswordInput} type="password" />
-//           </div>
-//           <br />
-//           <div>
-//             <div className="ui error message">
-//               {error.message && <div className="header">{error.message}</div>}
-//             </div>
-//             <button className="ui submit button" onClick={handleLoginClick}>
-//               Login
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LoginForm;
