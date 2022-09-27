3b1ed3962fd4184b6fbbfada873892327f202648
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-27 16:34:23 -0400

    remove 'required' from id, add email to schema

--- API/models/userModel.js
+++ API/models/userModel.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  id: { type: String, required: true },
+  id: { type: String },
   name: { type: String, required: true, maxLength: 500 },
+  email: { type: String, required: true, maxLength: 500 },
   password: { type: String, required: true, maxLength: 500 },
 });
 
