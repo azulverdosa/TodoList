fdee4b055fb7bbc9f41d6ec021ee3fc270ae286a
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-28 10:36:53 -0400

    update list model

--- API/models/listModel.js
+++ API/models/listModel.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ListSchema = new Schema({
-  id: { type: String, required: true },
+  id: { type: String, required: false },
   title: { type: String, required: true, maxLength: 500 },
+  note: { type: String, required: false, maxLength: 500 },
+  owner: { type: String, required: false },
 });
 
 const List = mongoose.model('List', ListSchema);
