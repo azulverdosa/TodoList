56123f9fd3d73792d21d9f4d80b329069adfc9ce
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-28 12:55:38 -0400

    edit 'List' component name

--- UI/src/App.js
+++ UI/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import List from './components/Lists/List';
-import Lists from './components/Lists/Lists';
+import List from './components/Lists/TasksList';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
+import Lists from './components/Lists/ListsList';
 
 const App = () => {
   return (
