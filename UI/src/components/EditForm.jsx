1abca322ab927d88e4c39ffc9aaacf6991172972
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-09-29 18:04:23 -0400

    create edit form component

---
+++ UI/src/components/EditForm.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import axios from 'axios';
+import urlJoin from 'url-join';
+
+const EditForm = ({ item, setItems, type, exitEditForm, updateList }) => {
+  const [newItem, setNewItem] = useState(item);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    const { name, value } = event.target;
+    setNewItem((prevTasks) => {
+      return {
+        ...prevTasks,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleEditSubmit = (event) => {
+    event.preventDefault();
+
+    //need a way to change btwn task & list with details
+    axios
+      .post(urlJoin(process.env.REACT_APP_API_URL, 'task', item._id), {
+        title: newItem.title,
+        note: newItem.note,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          exitEditForm();
+          updateList(res.data);
+        } else {
+          throw res;
+        }
+      })
+      .catch((err) => {
+        console.log('error?', err);
+      });
+  };
+
+  const handleCancel = (event) => {
+    event.preventDefault();
+    exitEditForm();
+  };
+
+  return (
+    <form className="ui form" style={{ margin: '20px' }}>
+      <div className="field">
+        <input onChange={handleChange} name="title" value={newItem.title} />
+      </div>
+      <div className="field">
+        <textarea value={newItem.note} name="note" onChange={handleChange} rows="2"></textarea>
+      </div>
+      <button onClick={handleEditSubmit} className="ui compact button">
+        Save
+      </button>
+      <button className="ui compact button" onClick={handleCancel}>
+        Cancel
+      </button>
+    </form>
+  );
+};
+
+export default EditForm;
