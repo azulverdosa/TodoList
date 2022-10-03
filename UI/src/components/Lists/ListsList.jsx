b412cdaa04ac107e9102956461b0869409627b7a
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-03 09:53:40 -0400

    refactor ListsList to handle more complexity

--- UI/src/components/Lists/ListsList.jsx
+++ UI/src/components/Lists/ListsList.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import urlJoin from 'url-join';
+import axios from 'axios';
+
 import AddList from './AddList';
 import ListItem from './ListItem';
 
-const Lists = () => {
+const ListsList = () => {
   const [isEditing, setIsEditing] = useState(false);
-  const [lists, setLists] = useState([
-    {
-      title: '',
-      note: '',
-    },
-  ]);
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    isEditing ||
+      axios
+        .get(urlJoin(process.env.REACT_APP_API_URL, 'list'))
+        .then((res) => {
+          if (res.status === 200) {
+            setLists(
+              res.data.map((list) => ({
+                ...list,
+                title: list.title,
+              }))
+            );
+          } else {
+            throw res;
+          }
+        })
+        .catch((err) => {
+          console.log('error?', err);
+        });
+  }, [isEditing]);
 
   return (
     <div style={{ margin: '20px' }}>
@@ -22,10 +42,20 @@ const Lists = () => {
         <i className="edit icon" />
       </button>
       <h3>Your Lists</h3>
-      <ListItem lists={lists} setLists={setLists} isEditing={isEditing} />
+
+      {lists.map((list) => (
+        <ListItem
+          key={nanoid(10)}
+          list={list}
+          setLists={setLists}
+          isEditing={isEditing}
+          updateList={setLists}
+        />
+      ))}
+
       <AddList setLists={setLists} />
     </div>
   );
 };
 
-export default Lists;
+export default ListsList;
