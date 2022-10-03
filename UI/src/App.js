d7cdb05b5c15fa66c3734b1a5419720388ea0dce
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-03 09:51:47 -0400

    edit route paths and components

--- UI/src/App.js
+++ UI/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import List from './components/Lists/TasksList';
+import TasksList from './components/Lists/TasksList';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
-import Lists from './components/Lists/ListsList';
+import ListsList from './components/Lists/ListsList';
 
 const App = () => {
   return (
@@ -16,8 +16,8 @@ const App = () => {
         <div>
           <Routes>
             <Route path="/" exact element={<Home />} />
-            <Route path="/list/:id" exact element={<List />} />
-            <Route path="/lists" exact element={<Lists />} />
+            <Route path="/list" exact element={<TasksList />} />
+            <Route path="/lists" exact element={<ListsList />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/register" exact element={<Register />} />
 
