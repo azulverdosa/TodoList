06b5e647654533c38b32002d4f87c30a31d98793
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-03 09:53:20 -0400

    refactor ListItem to contain les complexity

--- UI/src/components/Lists/ListItem.jsx
+++ UI/src/components/Lists/ListItem.jsx
@@ -2,32 +2,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import urlJoin from 'url-join';
 import { nanoid } from 'nanoid';
+
 import EditForm from '../EditForm';
 
-const ListItem = ({ lists, setLists, isEditing }) => {
+const ListItem = ({ list, setLists, isEditing, updateList }) => {
   const [editOn, setEditOn] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get(urlJoin(process.env.REACT_APP_API_URL, 'lists'))
-      .then((res) => {
-        if (res.status === 200) {
-          setLists(res.data);
-        } else {
-          throw res;
-        }
-      })
-      .catch((err) => {
-        console.log('error?', err);
-      });
-  }, []);
 
   const handleDeleteItem = (id) => {
     axios
       .delete(urlJoin(process.env.REACT_APP_API_URL, 'lists', id))
       .then((res) => {
         if (res.status === 200) {
-          setLists(res.data);
+          updateList(res.data);
         } else {
           throw res;
         }
@@ -39,72 +25,53 @@ const ListItem = ({ lists, setLists, isEditing }) => {
 
   const handleEditItem = (id) => {
     setEditOn(!editOn);
-
-    console.log(id);
   };
 
-  const editListTemplate = (
-    <div>
-      {lists.map((list) => (
-        <ul key={nanoid(10)}>
-          <div>
-            {editOn ? (
-              <EditForm
-                id={list._id}
-                name={list.title}
-                note={list.note}
-                lists={lists}
-                setLists={setLists}
-                handleEditItem={handleEditItem}
-              />
-            ) : (
-              <>
-                <label>{list.title}</label>
-                <p>{list.note}</p>
-              </>
-            )}
-          </div>
-          <div>
-            {!editOn && (
-              <button
-                onClick={() => {
-                  handleEditItem(list._id);
-                }}
-                className="ui compact icon floated button"
-              >
-                <i className="edit icon" />
-              </button>
-            )}
-            {!editOn && (
-              <button
-                onClick={() => {
-                  handleDeleteItem(list._id);
-                }}
-                className="ui compact icon floated button"
-              >
-                <i className="trash icon" />
-              </button>
-            )}
-          </div>
-        </ul>
-      ))}
-    </div>
+  const editListTemplate = editOn ? (
+    <EditForm
+      item={list}
+      setLists={setLists}
+      exitEditForm={handleEditItem}
+      updateList={updateList}
+    />
+  ) : (
+    <>
+      <label>{list.title}</label>
+      <p>{list.note}</p>
+      <div>
+        <button
+          onClick={() => {
+            handleEditItem(list._id);
+          }}
+          className="ui compact icon floated button"
+        >
+          <i className="edit icon" />
+        </button>
+
+        <button
+          onClick={() => {
+            handleDeleteItem(list._id);
+          }}
+          className="ui compact icon floated button"
+        >
+          <i className="trash icon" />
+        </button>
+      </div>
+    </>
   );
 
   const viewListTemplate = (
-    <div style={{ margin: '20px' }}>
-      {lists.map((list) => (
-        <div className="ui list">
-          <div className="item">
-            <div className="header">{list.title}</div>
-            <div className="description">{list.note}</div>
-          </div>
+    <div>
+      <div className="ui list">
+        <div className="item">
+          <h5 className="header">{list.title}</h5>
+          <p className="description">{list.note}</p>
         </div>
-      ))}
+      </div>
     </div>
   );
 
-  return <div> {isEditing ? editListTemplate : viewListTemplate}</div>;
+  return <div style={{ margin: '20px' }}> {isEditing ? editListTemplate : viewListTemplate}</div>;
 };
 
 export default ListItem;
