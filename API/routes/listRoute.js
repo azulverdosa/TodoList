3bf0748ad29d7832a7a1b1ac986e1ddc3816c472
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-05 10:06:40 -0400

    edit getAllTasks function to only get list specific tasks, add get route to render list specific tasks & add delete route to delete a list

--- API/routes/listRoute.js
+++ API/routes/listRoute.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const List = require('../models/listModel');
+const Task = require('../models/taskModel');
 
 const getAllLists = (res) => List.find().then((foundLists) => res.json(foundLists));
+const getAllTasks = async (res, ids = []) => {
+  const filter = ids?.length ? { _id: { $in: ids } } : {};
+  const foundTasks = await Task.find(filter);
 
+  const tasks = foundTasks.map((doc) => {
+    const task = doc.toObject();
+
+    return {
+      ...task,
+      title: task.title || task.task,
+    };
+  });
+
+  res.json(tasks);
+};
 router.route('/').get((req, res) => {
   getAllLists(res);
 });
@@ -11,10 +26,12 @@ router.route('/').get((req, res) => {
 router.route('/').post((req, res) => {
   const title = req.body.title;
   const note = req.body.note;
+  const listId = req.body.listId;
 
   const newList = new List({
     title,
     note,
+    listId,
   });
 
   newList.save().then(() => {
@@ -22,17 +39,23 @@ router.route('/').post((req, res) => {
   });
 });
 
-router.route('/:listId').delete((req, res) => {
+router.route('/:listId').get(async (req, res) => {
   const listId = req.params.listId;
 
-  List.findByIdAndDelete(listId)
-    .then(() => {
-      getAllLists(res);
-    })
-    .catch((err) => {
-      console.log(`Failed deleteing task ${listId}`, err);
-      res.sendStatus(500);
-    });
+  if (listId) {
+    try {
+      const list = await List.findById(listId);
+
+      const tasks = list.tasks;
+
+      return getAllTasks(res, tasks);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send('Oops! Something happened!');
+    }
+  }
+
+  return res.status(400).send('need a list id!');
 });
 
 router.route('/:listId').post((req, res) => {
@@ -50,6 +73,19 @@ router.route('/:listId').post((req, res) => {
       res.sendStatus(500);
     });
   console.log('meow');
+});
+
+router.route('/:listId').delete((req, res) => {
+  const listId = req.params.listId;
+
+  List.findByIdAndDelete(listId)
+    .then(() => {
+      getAllLists(res);
+    })
+    .catch((err) => {
+      console.log(`Failed deleteing task ${listId}`, err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
