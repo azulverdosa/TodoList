909581a26e99f2044292a00acdba8deb8b87766c
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-05 10:09:46 -0400

    refactor getAllTasks function, add listId to .post route to add task to list array, finish .post & .delete to edit and delete a task

--- API/routes/taskRoute.js
+++ API/routes/taskRoute.js
@@ -1,27 +1,73 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/taskModel');
+const List = require('../models/listModel');
 
-const getAllTasks = (res) => Task.find().then((foundTasks) => res.json(foundTasks));
+const getAllTasks = async (res, ids = []) => {
+  const filter = ids?.length ? { _id: { $in: ids } } : {};
+  const foundTasks = await Task.find(filter);
 
-router.route('/').get((req, res) => {
-  getAllTasks(res);
+  const tasks = foundTasks.map((doc) => {
+    const task = doc.toObject();
+
+    return {
+      ...task,
+      title: task.title || task.task,
+    };
+  });
+
+  res.json(tasks);
+};
+
+router.route('/').post(async (req, res) => {
+  const title = req.body.title;
+  const note = req.body.note;
+  const listId = req.body.listId;
+  const completed = req.body.completed;
+
+  if (listId) {
+    try {
+      const list = await List.findById(listId);
+
+      const newTask = new Task({
+        title,
+        note,
+        completed,
+      });
+
+      const task = await newTask.save();
+
+      const newTasks = list.tasks.concat(task._id);
+
+      await list.update({
+        tasks: newTasks,
+      });
+
+      return getAllTasks(res, newTasks);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).send('Oops! Something happened!');
+    }
+  }
+  return res.status(400).send('need a list id!');
 });
 
-router.route('/').post((req, res) => {
+router.route('/:taskId').post((req, res) => {
+  const taskId = req.params.taskId;
   const title = req.body.title;
   const note = req.body.note;
   const completed = req.body.completed;
 
-  const newTask = new Task({
-    title,
-    note,
-    completed,
-  });
-
-  newTask.save().then(() => {
-    getAllTasks(res);
-  });
+  Task.findByIdAndUpdate(taskId, { title, note, completed })
+    .then((blah) => {
+      console.log(blah);
+      getAllTasks(res);
+    })
+    .catch((err) => {
+      console.log(`Failed to update task ${taskId}`, err);
+      res.sendStatus(500);
+    });
+  console.log('meow');
 });
 
 router.route(`/:taskId`).delete((req, res) => {
@@ -35,24 +81,6 @@ router.route(`/:taskId`).delete((req, res) => {
       console.log(`Failed deleting task ${taskId}`, err);
       res.sendStatus(500);
     });
-});
-
-router.route('/:taskId').post((req, res) => {
-  const taskId = req.params.taskId;
-  const title = req.body.title;
-  const note = req.body.note;
-
-  //I want this to update any changes - or just update all as if one doesn't change it's the same anyway
-  Task.findByIdAndUpdate(taskId, { title, note })
-    .then((blah) => {
-      console.log(blah);
-      getAllTasks(res);
-    })
-    .catch((err) => {
-      console.log(`Failed to update task ${taskId}`, err);
-      res.sendStatus(500);
-    });
-  console.log('meow');
 });
 
 module.exports = router;
