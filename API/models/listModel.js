22a9be0e16ec21de74e8e5ad11f4cbd49ef89412
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-05 10:04:45 -0400

    add tasks to schema

--- API/models/listModel.js
+++ API/models/listModel.js
@@ -6,6 +6,7 @@ const ListSchema = new Schema({
   id: { type: String, required: false },
   title: { type: String, required: true, maxLength: 500 },
   note: { type: String, required: false, maxLength: 500 },
+  tasks: { type: Array, required: false },
   owner: { type: String, required: false },
 });
 
