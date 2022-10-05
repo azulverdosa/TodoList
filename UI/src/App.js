9b3325309eeb4333b760682f308fb4428ff174df
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-05 10:10:18 -0400

    edit component name imports, remove unused route

--- UI/src/App.js
+++ UI/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
-import TasksList from './components/Lists/TasksList';
+import List from './components/Lists/TasksList';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
-import ListsList from './components/Lists/ListsList';
+import Lists from './components/Lists/Lists';
 
 const App = () => {
   return (
@@ -16,12 +16,10 @@ const App = () => {
         <div>
           <Routes>
             <Route path="/" exact element={<Home />} />
-            <Route path="/list" exact element={<TasksList />} />
-            <Route path="/lists" exact element={<ListsList />} />
+            <Route path="/list/:listId" exact element={<List />} />
+            <Route path="/lists" exact element={<Lists />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/register" exact element={<Register />} />
-
-            {/* <Route path="/login" exact element={<LoginForm />} /> */}
           </Routes>
         </div>
       </Router>
