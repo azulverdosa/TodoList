575443f8da36d05ff8f301bf7d18df04426e0946
AvaCado <91709114+azulverdosa@users.noreply.github.com>
2022-10-12 14:02:05 -0400

    refactor home component to

--- UI/src/components/Home.jsx
+++ UI/src/components/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import urlJoin from 'url-join';
+import { Button, Modal } from 'semantic-ui-react';
 
-import EditForm from './EditForm';
+import ModalExampleBasic from './Modal';
 
 const Home = () => {
+  const handleDeleteItem = (id) => {
+    axios
+      .delete(urlJoin(process.env.REACT_APP_API_URL, 'list', id))
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('deleted!');
+        } else {
+          throw res;
+        }
+      })
+      .catch((err) => {
+        console.log('error?', err);
+      });
+  };
+
   return (
     <div style={{ margin: '20px' }}>
       <h5>You look like you might need to do something, let us help you with that 😉</h5>
@@ -16,6 +34,24 @@ const Home = () => {
       </Link>
       <br />
       <h5>Let's play around a bit:</h5>
+      <Button className="ui compact left floated button">Delete</Button>
+
+      <Button className="ui compact left floated button" as="a">
+        Save
+      </Button>
+
+      <Modal
+        trigger={
+          <Button className="ui compact left floated button">
+            <i className="trash icon" />
+          </Button>
+        }
+        header="DELETE"
+        content="Are you sure you want to delete this?"
+        actions={['Cancel', { key: 'done', content: 'Yes', positive: true }]}
+      />
+
+      <ModalExampleBasic />
       <br />
     </div>
   );
