1a64e2f0e356083699804b8c11322a537956a081
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-10-25 17:32:18 -0400

    add logout button and give onClick functionality

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -1,8 +1,40 @@
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import urlJoin from 'url-join';
 
 const Header = () => {
+  const [loginStatus, setLoginStatus] = useState(true);
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+
+    axios
+      .get(urlJoin(process.env.REACT_APP_API_URL, 'logout'), { withCredentials: true })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('logged Out');
+          setLoginStatus(false);
+          console.log('res.data :>> ', res.data);
+          navigate('/');
+        } else {
+          console.log('not error but will throw');
+          throw res;
+        }
+      })
+      .catch((err) => {
+        console.log('error?', err);
+      });
+  };
+
+  console.log('header loginStatus :>> ', loginStatus);
+
   return (
     <h2 className="ui block header">
+      <button onClick={handleLogout} className="ui compact right floated button">
+        Log Out
+      </button>
       <Link to="/login">
         <button className="ui compact right floated button">Login</button>
       </Link>
