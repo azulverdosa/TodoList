53dce39175717974570253287ab646d5b54c3b4e
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-08 14:15:17 -0500

    add protected route

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -8,12 +8,29 @@ import List from './components/Lists/TasksList';
 import Login from './components/User/Login';
 import Register from './components/User/Register';
 import Lists from './components/Lists/Lists';
+import Profile from './components/User/Profile';
+import Protected from './components/User/Protected';
 
 const App = () => {
+  const [loggedInStatus, setLoggedInStatus] = useState(null);
+
+  const logIn = () => {
+    setLoggedInStatus(true);
+  };
+
+  const logOut = () => {
+    setLoggedInStatus(false);
+  };
+
   return (
-    <div>
+    <main>
       <Router>
         <Header />
+        {loggedInStatus ? (
+          <button onClick={logOut}>Logout</button>
+        ) : (
+          <button onClick={logIn}>Login</button>
+        )}
         <div>
           <Routes>
             <Route path="/" exact element={<Home />} />
@@ -21,11 +38,20 @@ const App = () => {
             <Route path="/lists" exact element={<Lists />} />
             <Route path="/login" exact element={<Login />} />
             <Route path="/register" exact element={<Register />} />
+            <Route
+              path="/profile"
+              exact
+              element={
+                <Protected loggedInStatus={loggedInStatus}>
+                  <Profile />
+                </Protected>
+              }
+            />
           </Routes>
         </div>
         <Footer />
       </Router>
-    </div>
+    </main>
   );
 };
 
