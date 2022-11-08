a1f09846e910e47de15e6db7a8bb2ebff939049c
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-08 14:13:45 -0500

    add notes fo future use

--- API/models/userModel.js
+++ API/models/userModel.js
@@ -7,6 +7,12 @@ const UserSchema = new Schema({
   name: { type: String, required: true, maxLength: 500 },
   email: { type: String, required: true, maxLength: 500 },
   password: { type: String, required: true, maxLength: 500 },
+  // roles: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Role',
+  //   },
+  // ],
 });
 
 const User = mongoose.model('User', UserSchema);
