b0a4d5c1ea43a226a01dca4b40b268128daca287
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-08 14:14:21 -0500

    move route handler function to controllers

--- API/routes/listRoute.js
+++ API/routes/listRoute.js
@@ -3,124 +3,16 @@ const router = express.Router();
 
 const List = require('../models/listModel');
 
-const { deleteTasksFromDB, getListsFromDB, getTasksFromDB } = require('../helpers/databaseHelpers');
+const {
+  getAllLists,
+  addNewList,
+  getList,
+  editList,
+  deleteList,
+} = require('../controllers/listControllers');
 
-router.route('/').get(async (req, res) => {
-  try {
-    const lists = await getListsFromDB();
-    return res.json(lists);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send('Oops! Something happened!');
-  }
-});
+router.route('/').get(getAllLists).post(addNewList);
 
-router.route('/').post((req, res) => {
-  const title = req.body.title;
-  const note = req.body.note;
-
-  const newList = new List({
-    note,
-    tasks: [],
-    title,
-  });
-
-  newList.save().then(async () => {
-    try {
-      const lists = await getListsFromDB();
-
-      return res.json(lists);
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send('Oops! Something happened!');
-    }
-  });
-});
-
-router.route('/:listId').get(async (req, res) => {
-  const listId = req.params.listId;
-
-  if (listId) {
-    try {
-      const lists = await getListsFromDB(listId);
-      const listName = await lists[0].title;
-
-      if (lists?.length) {
-        const taskIds = lists?.[0]?.tasks;
-
-        if (taskIds.length) {
-          const tasks = await getTasksFromDB(taskIds);
-          return res.json({
-            listName,
-            tasks,
-          });
-        }
-
-        return res.send([]);
-      }
-
-      return res.sendStatus(400).send('No list found with that ID');
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send('Oops! Something happened!');
-    }
-  }
-
-  return res.status(400).send('need a list id!');
-});
-
-router.route('/:listId').post((req, res) => {
-  const listId = req.params.listId;
-  const list = req.body.list;
-  const note = req.body.note;
-
-  List.findByIdAndUpdate(listId, { list: list, note: note })
-    .then(async () => {
-      try {
-        const lists = await getListsFromDB();
-        return res.json(lists);
-      } catch (err) {
-        console.error(err);
-        return res.status(500).send('Oops! Something happened!');
-      }
-    })
-    .catch((err) => {
-      console.log(`Failed to update list ${listId}`, err);
-      res.sendStatus(500);
-    });
-});
-
-router.route('/:listId').delete(async (req, res) => {
-  const listId = req.params.listId;
-
-  if (listId) {
-    try {
-      const list = await List.findById(listId);
-      const tasks = list.tasks;
-
-      tasks.length && (await deleteTasksFromDB(tasks));
-
-      return List.findByIdAndDelete(listId)
-        .then(async () => {
-          try {
-            const lists = await getListsFromDB();
-            return res.json(lists);
-          } catch (err) {
-            console.error(err);
-            return res.status(500).send('Oops! Something happened!');
-          }
-        })
-        .catch((err) => {
-          console.log(`Failed deleteing task ${listId}`, err);
-          res.sendStatus(500);
-        });
-    } catch (err) {
-      console.error(err);
-      return res.status(500).send('Oops! Something happened!');
-    }
-  }
-
-  return res.status(400).send('need a list id!');
-});
+router.route('/:listId').get(getList).post(editList).delete(deleteList);
 
 module.exports = router;
--- API/routes/userRoute.js
+++ API/routes/userRoute.js
@@ -4,91 +4,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
-const User = require('../models/userModel');
+const { userLogin, userLogout, userRegistration } = require('../controllers/userControllers');
 
-const findUserByEmail = (email) => User.findOne({ email });
+router.route('/login').post(userLogin);
 
-router.route('/login').post(async (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
+router.route('/register').post(userRegistration);
 
-  const foundUser = await findUserByEmail(email);
-  const userHashedId = bcrypt.hashSync(foundUser._id.toString(), bcrypt.genSaltSync());
-
-  if (foundUser) {
-    const accessToken = await jwt.sign({ email }, process.env.ACCESS_SECRET, {
-      expiresIn: '2m',
-    });
-
-    const refreshToken = await jwt.sign({ email: email }, process.env.REFRESH_SECRET, {
-      expiresIn: '10m',
-    });
-
-    try {
-      const doesPasswordMatch = await bcrypt.compareSync(password, foundUser.password);
-
-      if (doesPasswordMatch) {
-        // req.session.user = foundUser;
-        // res.cookie('token', userHashedId);
-        // console.log('Cookies: ', req.cookies);
-        // console.log('session: ', req.session);
-
-        return res.json({
-          cookieStatus: 'cookie set',
-          userHashedId: userHashedId,
-          accessToken,
-          refreshToken,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-
-      return res.status(500).send('Oops! Something happened!');
-    }
-  }
-
-  console.log('No User Found');
-  return res.status(400).send('No User Found');
-});
-
-router.route('/register').post(async (req, res) => {
-  const name = req.body.name;
-  const password = req.body.password;
-  const email = req.body.email;
-
-  const hashedPassword = await bcrypt.hashSync(password, bcrypt.genSaltSync());
-
-  findUserByEmail(email).then((user) => {
-    if (user) {
-      return res.status(400).send('Email already in use');
-    }
-
-    const newUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    res.cookie('token', newUser._id);
-    req.session.newUser = newUser;
-
-    newUser
-      .save()
-      .then(() => {
-        return res.sendStatus(200);
-      })
-      .catch(() => {
-        return res.sendStatus(500);
-      });
-  });
-});
-
-router.route('/logout').get(async (req, res) => {
-  res.clearCookie('token');
-  res.send("cookie 'token' cleared");
-  req.session.destroy();
-  req.session = null;
-});
+router.route('/logout').get(userLogout);
 
 module.exports = router;
