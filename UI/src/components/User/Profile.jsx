9bbe4cca17057f2003dfa3c8e029c0a4479376b8
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-08 14:16:22 -0500

    create Profile component

---
+++ UI/src/components/User/Profile.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import urlJoin from 'url-join';
+
+const Profile = () => {
+  const [loginStatus, setLoginStatus] = useState(true);
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+
+    axios
+      .get(urlJoin(process.env.REACT_APP_API_URL, 'logout'), { withCredentials: true })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log('logged Out');
+          setLoginStatus(false);
+          console.log('res.data :>> ', res.data);
+          navigate('/');
+        } else {
+          console.log('not error but will throw');
+          throw res;
+        }
+      })
+      .catch((err) => {
+        console.log('error?', err);
+      });
+  };
+
+  console.log('loginStatus :>> ', loginStatus);
+
+  return (
+    <main>
+      <h1 style={{ textAlign: 'center' }}>Hello there 'user Name'!</h1>
+      <img
+        style={{ margin: '20px' }}
+        alt="avatar"
+        className="ui avatar image"
+        src="https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Penguin-512.png"
+      />
+      <span style={{ fontWeight: 'bold' }}>Username</span>
+      <br />
+
+      <span style={{ marginLeft: '20px' }}>Desription</span>
+      <br />
+      <br />
+      <button
+        style={{ marginLeft: '20px' }}
+        onClick={handleLogout}
+        className="ui compact floated button"
+      >
+        Edit Profile
+      </button>
+      <Link to="/lists">
+        <button className="ui compact floated button">Your Todo Lists</button>
+      </Link>
+      <br />
+      <button
+        style={{ margin: '20px' }}
+        onClick={handleLogout}
+        className="ui compact floated button"
+      >
+        Log Out
+      </button>
+    </main>
+  );
+};
+export default Profile;
