e60de875c1b738c802da539ffcae73be63b14284
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-15 13:15:19 -0500

    add conditional buttons, remove testing buttons, import props

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import urlJoin from 'url-join';
 
-const Header = () => {
-  const [loginStatus, setLoginStatus] = useState(true);
+const Header = ({ setLogIn, setLogOut, loggedInStatus, setLoginStatus }) => {
   const navigate = useNavigate();
 
   const handleLogout = (event) => {
@@ -28,26 +27,39 @@ const Header = () => {
       });
   };
 
-  console.log('header loginStatus :>> ', loginStatus);
-
   return (
     <h2 className="ui block header">
-      <button onClick={handleLogout} className="ui compact right floated button">
+      {/* <button onClick={handleLogout} className="ui compact right floated button">
         Log Out
-      </button>
-      <Link to="/login">
+      </button> */}
+      {loggedInStatus ? (
+        <button onClick={setLogOut} className="ui compact right floated button">
+          Logout
+        </button>
+      ) : (
+        <button onClick={setLogIn} className="ui compact right floated button">
+          Login
+        </button>
+      )}
+      {/* <Link to="/login">
         <button className="ui compact right floated button">Login</button>
-      </Link>
-      <Link to="/register">
+      </Link> */}
+      {/* <Link to="/register">
         <button className="ui compact right floated button">Register</button>
-      </Link>
+      </Link> */}
       <Link to="/">
         <button className="ui compact right floated button">Home</button>
       </Link>
-      <Link to="/lists">
-        <button className="ui compact right floated button">Your Lists</button>
-      </Link>
-
+      {loggedInStatus && (
+        <Link to="/lists">
+          <button className="ui compact right floated button">Your Lists</button>
+        </Link>
+      )}
+      {loggedInStatus && (
+        <Link to="/profile">
+          <button className="ui compact right floated button">Profile</button>
+        </Link>
+      )}
       <i className="list ul icon"></i>
       <div className="content">
         TodoMatic
