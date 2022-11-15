b706e09b3377307063afb9a0687f29d5b420e8e8
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-15 13:13:25 -0500

    add http req & upadte style

--- UI/src/components/User/Login.jsx
+++ UI/src/components/User/Login.jsx
@@ -1,47 +1,72 @@
 import React, { useState } from 'react';
+import { Redirect, Navigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import urlJoin from 'url-join';
 
+// import { axiosAuth } from '../../helpers/fetchWithAuth';
+
 const Login = () => {
-  const [login, setLogin] = useState({
-    name: '',
+  const [loginInfo, setLoginInfo] = useState({
+    email: '',
     password: '',
   });
   const [loginStatus, setLoginStatus] = useState(false);
+  const [loggedInUser, setLoggedInUser] = useState({});
   const [error, setError] = useState({ message: '' });
 
   const handleInputChange = (event) => {
     event.preventDefault();
 
     const { name, value } = event.target;
-    setLogin((prev) => {
+    setLoginInfo((prev) => {
       return {
         ...prev,
         [name]: value,
       };
     });
-    console.log(login);
+    // console.log('loginInfo :>> ', loginInfo);
   };
 
   const handleLoginClick = (event) => {
     event.preventDefault();
-    if (login.name === '' || login.password === '') {
+
+    if (loginInfo.email === '' || loginInfo.password === '') {
       setError({ message: 'Feilds cannot be blank' });
+    } else if (loginInfo.email && loginInfo.password) {
+      axios
+        .post(urlJoin(process.env.REACT_APP_API_URL, 'auth'), loginInfo, {
+          // headers: {authorization: `Bearer ${jwt}`},
+          withCredentials: true,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            console.log('LOG IN SUCCESS');
+            console.log('res.data :>> ', res.data);
+            setLoginStatus(true);
+            setLoggedInUser(res.data);
+          } else {
+            throw res;
+          }
+        })
+        .catch((err) => {
+          console.log('error :>> ', err);
+        });
     }
-    setLoginStatus(true);
   };
 
-  return (
-    <div className="ui large form error" style={{ margin: '20px' }}>
-      <div className="two fields">
-        <form style={{ padding: 30 }}>
+  const loggedOutTemplate = (
+    <main className="ui large form error" style={{ display: 'flex', flexDirection: 'column' }}>
+      <h3 style={{ margin: 0 }}>Sign in </h3>
+      <div style={{ margin: '0px 20px 20px 0px' }} className="two fields">
+        <form style={{ padding: '0px 10px 10px 10px' }}>
+          <br />
           <div className="field">
-            <label autoComplete="off">Name</label>
+            <label autoComplete="off">Email</label>
             <input
-              name="name"
-              value={login.name}
+              name="email"
+              value={loginInfo.email}
               onChange={handleInputChange}
-              placeholder="Name"
+              placeholder="Email"
               type="text"
             />
           </div>
@@ -50,13 +75,13 @@ const Login = () => {
             <label autoComplete="off">Password</label>
             <input
               name="password"
-              value={login.password}
+              value={loginInfo.password}
               onChange={handleInputChange}
               placeholder="Password"
               type="password"
             />
           </div>
-          <br />
+
           <div>
             <div className="ui error message">
               {error.message && <div className="header">{error.message}</div>}
@@ -68,8 +93,16 @@ const Login = () => {
           </button>
         </form>
       </div>
-    </div>
+      <p>
+        Need an account? <br />
+        <Link to="/register">
+          <span>Register Here</span>
+        </Link>
+      </p>
+    </main>
   );
+
+  return <div>{loginStatus ? <Navigate replace to="/profile" /> : loggedOutTemplate}</div>;
 };
 
 export default Login;
