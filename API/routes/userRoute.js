f3341a66a4cef7804d755809da590cacbda3636f
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-25 08:38:17 -0500

    add refresh endpoint bring in verifyJWT

--- API/routes/userRoute.js
+++ API/routes/userRoute.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
 
-const { userLogin, userLogout, userRegistration } = require('../controllers/userControllers');
+const verifyJWT = require('../middlewares/verifyJWT');
+
+const {
+  userLogin,
+  userLogout,
+  userRegistration,
+  userRefreshToken,
+} = require('../controllers/userControllers');
 
 router.route('/login').post(userLogin);
 
 router.route('/register').post(userRegistration);
 
 router.route('/logout').get(userLogout);
+
+router.route('/refresh').get(userRefreshToken);
 
 module.exports = router;
