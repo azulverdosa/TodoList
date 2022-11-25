929bd49d17fab0885082026c4eab2ad2b9c84acf
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-25 08:37:35 -0500

    import AuthContextProvider

--- UI/src/App.js
+++ UI/src/App.js
@@ -10,46 +10,50 @@ import Register from './components/User/Register';
 import Lists from './components/Lists/Lists';
 import Profile from './components/User/Profile';
 import Protected from './components/User/Protected';
+import { AuthContextProvider } from './helpers/useAuthContext';
 
 const App = () => {
-  const [loggedInStatus, setLoggedInStatus] = useState(null);
+  const [loginStatus, setLoginStatus] = useState(null);
 
-  const logIn = () => {
-    setLoggedInStatus(true);
+  const setLogIn = () => {
+    setLoginStatus(true);
   };
 
-  const logOut = () => {
-    setLoggedInStatus(false);
+  const setLogOut = () => {
+    setLoginStatus(false);
   };
 
   return (
     <main>
       <Router>
-        <Header />
-        {loggedInStatus ? (
-          <button onClick={logOut}>Logout</button>
-        ) : (
-          <button onClick={logIn}>Login</button>
-        )}
-        <div>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/list/:listId" exact element={<List />} />
-            <Route path="/lists" exact element={<Lists />} />
-            <Route path="/login" exact element={<Login />} />
-            <Route path="/register" exact element={<Register />} />
-            <Route
-              path="/profile"
-              exact
-              element={
-                <Protected loggedInStatus={loggedInStatus}>
-                  <Profile />
-                </Protected>
-              }
-            />
-          </Routes>
-        </div>
-        <Footer />
+        <AuthContextProvider>
+          <Header
+            setLogIn={setLogIn}
+            setLogOut={setLogOut}
+            loggedInStatus={loginStatus}
+            setLoggedInStatus={setLoginStatus}
+          />
+
+          <div>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/list/:listId" exact element={<List />} />
+              <Route path="/lists" exact element={<Lists />} />
+              <Route path="/login" exact element={<Login />} />
+              <Route path="/register" exact element={<Register />} />
+              <Route
+                path="/profile"
+                exact
+                element={
+                  <Protected loggedInStatus={loginStatus}>
+                    <Profile />
+                  </Protected>
+                }
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </AuthContextProvider>
       </Router>
     </main>
   );
