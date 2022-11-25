c57ebe0e1d94b1927deae7d2e8de47bdd40e8eb0
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2022-11-25 08:39:44 -0500

    remove session connection, add routes

--- API/server.js
+++ API/server.js
@@ -16,37 +16,22 @@ app.use(cors(corsConfig)); //then use config
 app.options('*', cors(corsConfig));
 
 app.use(express.json());
-
-//for express cookie sessions
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { path: '/', httpOnly: true, secure: false, maxAge: 3600000 },
-    // httpOnly: true,
-    // maxAge: null, //can also be parseInt(process.env.MAX_AGE) - time recorded in miliseconds - 1000ms = 1s, 3600000ms = 1hr
-    //can also use 'expire' = res.cookie(name, 'value', {expire: 360000 + Date.now()}) - Expires after 360000 ms from the time it is set.
-  })
-);
-app.use((req, res, next) => {
-  console.log('req.session :>> ', req.session);
-  next();
-});
-
 app.use(cookieParser());
 
+//mongoose & mongoDB connection
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+  console.log('MongoDB database connection established successfully 👍');
 });
 
-//you custome routes
+//your custome routes
+//can require auth (protect) on specific routes by adding app.use(verifyJWT) specific routes
 app.use('/task', require('./routes/taskRoute'));
-app.use('/', require('./routes/userRoute'));
 app.use('/list', require('./routes/listRoute'));
+app.use('/auth', require('./routes/authRoute'));
+app.use('/', require('./routes/userRoute'));
 
 //server
 app.get('/hello', (req, res) => {
